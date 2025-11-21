3b3e431f217b8fcba723e666d8d41bb1b9cc05ca
yssfmlha <youssef_mlaouhia@hotmail.fr>
2025-11-21 15:37:41 +0100

    Update StudentManagementApplicationTests.java

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-})
 class StudentManagementApplicationTests {
 
     @Test
