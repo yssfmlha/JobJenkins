620b9f2e17b9a66ec8a928ae07f7dcb72704b382
yssfmlha <youssef_mlaouhia@hotmail.fr>
2025-11-28 16:14:08 +0100

    Correction quelque probleme Maintenabilité

--- src/main/java/tn/esprit/studentmanagement/repositories/StudentRepository.java
+++ src/main/java/tn/esprit/studentmanagement/repositories/StudentRepository.java
@@ -2,7 +2,6 @@ package tn.esprit.studentmanagement.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import tn.esprit.studentmanagement.entities.Course;
 import tn.esprit.studentmanagement.entities.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
